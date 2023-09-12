19b99c5012076f8b6be091d71391523f2d9467e1
Supriyohcl <144807379+Supriyohcl@users.noreply.github.com>
2023-09-12 15:56:51 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 123456
+let accountEmail = "supriyo@sadhu.com"
+var accountPassword = "123456789"
+accountCity = "Burdwan"
+let accountState;
+
+accountEmail = "sup@sad.com"
+accountPassword ="654321"
+accountCity = "Kolkata"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
